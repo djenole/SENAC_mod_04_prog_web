6fa4dcfb2ca003d3b9cf1a0af28ced5ce477bc5c
Djenole Reis <djenole@gmail.com>
2022-09-09 03:19:13 -0300

    criado controller de turma

---
+++ projeto-springboot-djenole/src/main/java/com/spring/projetospringbootdjenole/controller/TurmaController.java
@@ -0,0 +1,20 @@
+package com.spring.projetospringbootdjenole.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.projetospringbootdjenole.service.TurmaService;
+
+public class TurmaController {
+     
+    @Autowired
+    private TurmaService turmaService;
+
+    @GetMapping("listaTurmas")
+    public ModelAndView listarTodasTurmas(){
+        ModelAndView mv = new ModelAndView("turma/listaTurmas");
+        mv.addObject("turmas", turmaService.buscarTodasTurmas());
+        return mv;
+    }
+}
