86cdbd5c1c95b6b4f21eb90062dbfdaa001b704c
Djenole Reis <djenole@gmail.com>
2022-09-09 03:18:33 -0300

    criado controller de professor

---
+++ projeto-springboot-djenole/src/main/java/com/spring/projetospringbootdjenole/controller/ProfessorController.java
@@ -0,0 +1,25 @@
+package com.spring.projetospringbootdjenole.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.projetospringbootdjenole.service.ProfessorService;
+
+@Controller
+@RequestMapping("professor")
+public class ProfessorController {
+
+    @Autowired
+    private ProfessorService professorService;
+
+    @GetMapping("listarProfessores")
+    public ModelAndView listarTodosProfessores() {
+        ModelAndView mv = new ModelAndView("professor/listaProfessores");
+        mv.addObject("professores", professorService.buscarTodosProfessores());
+        return mv;
+    }
+    
+}
