fd36e4551568ade881d996b1548784f433ce7bfd
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-10-06 20:33:51 -0300

    adicionado metodo buscarTodasTurmas()

--- projeto-springboot-djenole/src/main/java/com/spring/projetospringbootdjenole/controller/TurmaController.java
+++ projeto-springboot-djenole/src/main/java/com/spring/projetospringbootdjenole/controller/TurmaController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.projetospringbootdjenole.entity.Aluno;
+import com.spring.projetospringbootdjenole.entity.Turma;
 import com.spring.projetospringbootdjenole.service.TurmaService;
 
 public class TurmaController {
@@ -16,5 +18,14 @@ public class TurmaController {
         ModelAndView mv = new ModelAndView("turma/listaTurmasPage");
         mv.addObject("turmas", turmaService.buscarTodasTurmas());
         return mv;
+    }
+    
+    
+    @GetMapping("/cadastrarTurma")
+    public ModelAndView cadastrarTurma() {
+    	ModelAndView mv = new ModelAndView("turma/cadastrarTurma");
+    	mv.addObject("turma", new Turma());
+    	
+    	return mv;
     }
 }
