4ce385240dbb18aeeccf04cf3e09396d6824d8de
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-10-06 19:15:18 -0300

    implementado metodos get e set turma, e tambem, anotação manytoOne com joinColumn id_turma

--- projeto-springboot-djenole/src/main/java/com/spring/projetospringbootdjenole/entity/Aluno.java
+++ projeto-springboot-djenole/src/main/java/com/spring/projetospringbootdjenole/entity/Aluno.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Aluno implements Serializable {
@@ -15,6 +17,11 @@ public class Aluno implements Serializable {
 	@GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    
+    
+    @ManyToOne
+    @JoinColumn(name = "id_turma")
+    private Turma turma;
     
     public Integer getId() {
         return id;
@@ -29,6 +36,13 @@ public class Aluno implements Serializable {
         this.nome = nome;
     }
     
+    public Turma getTurma() {
+    	return turma;
+    }
+    
+    public void setTurma(Turma turma) {
+    	this.turma = turma;
+    }
 
     
 }
